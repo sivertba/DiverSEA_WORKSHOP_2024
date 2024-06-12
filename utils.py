9e6ca1437a30d664d856067c10204215db62ebde
sivertba <sivert.bakken@gmail.com>
2024-06-12 16:11:30 +0200

    Various fixes

--- utils.py
+++ utils.py
@@ -1,42 +1,51 @@
-from osgeo import gdal
 import numpy as np
-import os
+from osgeo import gdal
+from osgeo import osr
+from pathlib import Path
 
-def embed_geo_data(fname: str, tif_path: os.PathLike, geo_data: np.ndarray):
+from hypso.georeference.reference import generate_geotiff
+
+def embed_geo_data(fname: str, satObj, geo_data: np.ndarray) -> None:
     """
-    Embeds geographical data from a NumPy array into a new georeferenced TIF file.
+    Generate RGB GeoTiff image
 
-    Args:
-        fname (str):            Name of the output TIF file.
-        tif_path (os.PathLike): Path to the original TIF file.
-        geo_data (np.ndarray):  NumPy array containing geographical data with the 
-                                same spatial dimensions as the original TIF file.
+    :param satObj: Hypso satellite object.
+    :param overwrite: If true, overwrite the previously generated RGB GeoTiff image
+
+    :return: No return
     """
 
-    # Open the original TIF file
-    with gdal.Open(tif_path, gdal.GA_ReadOnly) as src_ds:
-        # Get information from the original file
-        driver = gdal.GetDriverByName('GTiff')
-        geotransform = src_ds.GetGeoTransform()
-        projection = src_ds.GetProjection()
-        cols, rows = src_ds.RasterXSize, src_ds.RasterYSize
 
-    # Create a new TIF file with the same dimensions and projection
-    if not fname.endswith('.tif'):
-        fname += '.tif'
-    out_ds = driver.Create(f"{tif_path[:-4]}_geo.tif", cols, rows, 1, gdal.GDT_Float32)
+    # GeoTiff Output ------------------------------------------------------------------------
+    top_folder_name = satObj.info["top_folder_name"]
+    geotiff_folder_path = Path(top_folder_name, "geotiff")
+    output_path_rgba_tif = Path(geotiff_folder_path, fname)
 
-    # Set geotransform and projection for the new file
-    out_ds.SetGeoTransform(geotransform)
-    out_ds.SetProjection(projection)
+    # Select data for RGB GeoTiff --------------------------------------------------------
+    geo_data = np.nan_to_num(geo_data)
+    cube_data = np.repeat(geo_data[:, :, np.newaxis], 3, axis=2)
 
-    # Write the geographical data to the new file
-    out_band = out_ds.GetRasterBand(1)
-    out_band.WriteArray(geo_data)
+    # Get Geometric Information----------------------------------------------------------------
+    grid_dims, geotransform, destination_epsg, grid_data_all_bands, _ = generate_geotiff(satObj, [0,1,2],
+                                                                                                    cube_data)
 
-    # Flush data to disk and close the file
-    out_band.FlushCache()
-    out_ds = None
+    # Geotiff Objects ------------------------------------------------------------------------------------------
+    # some Geotiff reference info:
+    # https://gis.stackexchange.com/questions/380607/how-to-geo-reference-a-tif-image-knowing-corner-coordinates
+    # https://gis.stackexchange.com/questions/275125/expressing-raster-rotation-with-gdal-geotransform-python
+
+    dst_ds_alpha_channel = gdal.GetDriverByName('GTiff').Create(
+        str(output_path_rgba_tif), grid_dims[0], grid_dims[1], 1, gdal.GDT_Float32)
+
+    dst_ds_alpha_channel.SetGeoTransform(geotransform)  # specify coords
+    srs = osr.SpatialReference()  # establish encoding
+    srs.ImportFromEPSG(destination_epsg)
+    dst_ds_alpha_channel.SetProjection(
+        srs.ExportToWkt())  # export coords to file
+    
+    data = grid_data_all_bands[:, :, 1]
+    dst_ds_alpha_channel.GetRasterBand(1).WriteArray(data)
+    dst_ds_alpha_channel.FlushCache()  # write to disk
 
 def check4files():
     import requests
